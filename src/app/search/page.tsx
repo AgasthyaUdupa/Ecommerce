b4841b7780e8c8214b6398789f0c1fd342731b0d
Agasthya Udupa <agasthyauduparajkumar2001@gmail.com>
2023-10-15 14:11:23 +0530

    sixth commit

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,38 +1,41 @@
-import ProductCard from "@/components/ProductCard"
-import { prisma } from "@/lib/db/prisma"
-import { Metadata } from "next"
-import { title } from "process"
+import ProductCard from "@/components/ProductCard";
+import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
 
-interface SearchPageProps{
-    searchParams :{ query : string}
+interface SearchPageProps {
+  searchParams: { query: string };
 }
 
-export function generateMetaData({searchParams:{query},}:SearchPageProps):Metadata{
-  return{
-    title:`Search: ${query}- Aguzon`
-  }
+export function generateMetadata({
+  searchParams: { query },
+}: SearchPageProps): Metadata {
+  return {
+    title: `Search: ${query} - Aguzon`,
+  };
 }
 
-export default async function SearchPage({searchParams :{query}}: SearchPageProps){
+export default async function SearchPage({
+  searchParams: { query },
+}: SearchPageProps) {
   const products = await prisma.product.findMany({
-    where:{
-        OR:[
-            {name:{contains: query,mode:"insensitive"}},
-            {description:{contains: query,mode:"insensitive"}},
-        ]
+    where: {
+      OR: [
+        { name: { contains: query, mode: "insensitive" } },
+        { description: { contains: query, mode: "insensitive" } },
+      ],
     },
-    orderBy:{id : "desc"}
-  })
+    orderBy: { id: "desc" },
+  });
 
-  if(products.length ===0){
-    return <div className="text-center">No Products Found</div>
+  if (products.length === 0) {
+    return <div className="text-center">No products found</div>;
   }
 
-  return(
+  return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-        {products.map(product =>(
-            <ProductCard product={product} key={product.id}/>
-        ))}
+      {products.map((product) => (
+        <ProductCard product={product} key={product.id} />
+      ))}
     </div>
-  )
+  );
 }
